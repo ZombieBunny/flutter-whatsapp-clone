3e3519ec378dd0cceadaad3dc19fc8c13b0a0315
Lee <leevashan.reddi@aax.co.za>
2019-09-24 13:58:31 +0200

    first commit

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,41 @@
+class ChatModel {
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name, this.message, this.time, this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+      name: "Hello World",
+      message: "bboom",
+      time: "10:40",
+      avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThTBWZRD214jQJXATZfd7ErmvMUBVnIZUV3gTvQ1zWVLVGyCY"),
+  new ChatModel(
+      name: "Hello World",
+      message: "bboom 2",
+      time: "10:46",
+      avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThTBWZRD214jQJXATZfd7ErmvMUBVnIZUV3gTvQ1zWVLVGyCY"),
+  new ChatModel(
+      name: "Hello World",
+      message: "bboom 3",
+      time: "10:47",
+      avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThTBWZRD214jQJXATZfd7ErmvMUBVnIZUV3gTvQ1zWVLVGyCY"),
+  new ChatModel(
+      name: "Hello World",
+      message: "bboom 4",
+      time: "10:48",
+      avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThTBWZRD214jQJXATZfd7ErmvMUBVnIZUV3gTvQ1zWVLVGyCY"),
+  new ChatModel(
+      name: "Hello World",
+      message: "bboom 5",
+      time: "10:49",
+      avatarUrl:
+          "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSThTBWZRD214jQJXATZfd7ErmvMUBVnIZUV3gTvQ1zWVLVGyCY"),
+];
